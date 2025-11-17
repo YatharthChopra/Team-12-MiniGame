a8d0de2b8c2e8154b9aa95a50ccbe69ce875e4bd
James-Belle <jamesbelle549@gmail.com>
2025-11-16 19:07:54 -0500

    trying to fix branch

--- unity-project/mini-game-collection/Assets/2025/Team12/Scripts/EnemyType.cs
+++
@@ -1,9 +0,0 @@
-namespace MiniGameCollection.Games2025.Team00
-{
-    public enum EnemyType
-    {
-        Level1_Green,
-        Level2_Orange,
-        Level3_Black,
-    }
-}
--- unity-project/mini-game-collection/Assets/2025/Team12/Scripts/PlayerID.cs
+++
@@ -1,8 +0,0 @@
-namespace MiniGameCollection.Games2025.Team00
-{
-    public enum PlayerID
-    {
-        Player1 = 0,
-        Player2 = 1,
-    }
-}
